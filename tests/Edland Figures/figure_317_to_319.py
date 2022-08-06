069efa1f9465c98d1df9d72efbd052cba785e367
ddmarshall <ddmarshall@gmail.com>
2022-08-05 23:27:07 -0700

    Generalized the xfoil dump reader and put in separate module.

--- tests/Edland Figures/figure_317_to_319.py
+++ tests/Edland Figures/figure_317_to_319.py
@@ -8,10 +8,10 @@ Created on Thu Aug  4 11:58:33 2022
 
 import numpy as np
 import matplotlib.pyplot as plt
-import csv
-from collections import namedtuple
 
 from pyBL.thwaites_method import ThwaitesSimData, ThwaitesSim
+
+from xfoil_interface import read_xfoil_dump_file
 
 def laminar_xfoil_comparison():
     """
@@ -26,11 +26,16 @@ def laminar_xfoil_comparison():
         None
     """
     
-    ## Case configuration
-#    npts = 161 # number of points for plotting
-    
     ## Load the XFoil data for the case
-    c, U_inf, Re, xfoil_upper, xfoil_lower, _ = get_laminar_xfoil_data('xfoil_laminar_dump.txt')
+    # parameters use to generate case
+    # NACA 0003
+    # aoa =0
+    # x_trans = 1 (upper and lower)
+    # Transition model n = 9
+    c = 1 #m
+    U_inf = 20 #m/s 
+    Re = 1000
+    xfoil_upper, xfoil_lower, _ = read_xfoil_dump_file('xfoil_laminar_dump.txt', U_inf)
     nu = U_inf*c/Re
 
     ## Create the simulations for the upper and lower surfaces
@@ -138,113 +143,13 @@ def laminar_xfoil_comparison():
     ax=axs[1]
     ax.plot(xfoil_lower.x[xfoil_lower.x>0.6], ts_l.du_edx(xfoil_lower.s[xfoil_lower.x>0.6]),label='XFOIL',color='k')
     ax.set(xlabel=('x(m)'))
-    ax.set(ylabel=(r'$\frac{du_e}{dx}$ (m/s$^2$)'))
+    ax.set(ylabel=(r'$u^{\prime}_e$ (m/s$^2$)'))
     ax.grid(True)
     
     fig.set_figwidth(8)
     fig.set_figheight(8)
-    plt.subplots_adjust(bottom=0.1, wspace=0.35)
+    plt.subplots_adjust(bottom=0.1, wspace=0.45)
     plt.show()
-
-
-def get_laminar_xfoil_data(xfoil_file):
-    """
-    Return the laminar results from XFoil for a NACA 0003 airfoil at -3 deg. 
-    angle of attack.
-    
-    Args
-    ----
-        xfoi_file - file name containing XFoil dump data
-        
-    Returns
-    -------
-        c - chord length
-        U_inf - freestream velocity
-        Re - frestream Reynolds number
-        s_lower - arclength distance from stagnation point on lower surface
-        s_upper - arclength distance from stagnation point on upper surface
-        Ue_lower - lower surface velocity
-        Ue_upper - upper surface velocity
-        del_star_lower - lower surface displacement thickness
-        del_star_upper - upper surface displacement thickness
-        theta_lower - lower surface momentum thickness
-        theta_upper - upper surface momentum thickness
-        cf_lower - lower surface skin friction coefficient
-        cf_upper - upper surface skin friction coefficient
-        H_lower - lower surface shape factor
-        H_upper - upper surface shape factor
-    """
-    ## Load the data from XFoil viscous dump file
-    # parameters use to generate case
-    # NACA 0003
-    # aoa =0
-    # x_trans = 1 (upper and lower)
-    # Transition model n = 9
-    c = 1 #m
-    U_inf = 20 #m/s 
-    Re = 1000
-    
-    PanelValues = namedtuple('PanelValues', ['s', 'x', 'y', 'Ue', 'delta_d', 'delta_m', 'cf', 'H', 'Hstar', 'P', 'm', 'K'])
-    WakeValues = namedtuple('PanelValues', ['s', 'x', 'y', 'Ue', 'delta_d', 'delta_m', 'H'])
-    
-    xfoil_file = open(xfoil_file, 'r')
-    xfoil_reader = csv.reader(xfoil_file)
-    panel_terms = []
-    wake_terms = []
-    for row in xfoil_reader:
-        if (row[0][0] != '#'):
-            col = np.array(row[0].split(), 'float')
-            
-            if (np.size(col) == 12):
-                panel_terms.append(col)
-            elif (np.size(col) == 8):
-                wake_terms.append(col)
-            else:
-                raise Exception('Invalid number of columns for XFoil import')
-                    
-    ## Find stagnation point and the total arclength of airfoil
-    panel_terms = np.asarray(panel_terms)
-    last_upper_idx = np.where(np.sign(panel_terms[:-1, 3]) != np.sign(panel_terms[1:, 3]))[0][0]
-    s_af = panel_terms[-1, 0]
-    
-    # interpolate stagnation point
-    dU = panel_terms[last_upper_idx+1, 3]-panel_terms[last_upper_idx, 3]
-    frac = -panel_terms[last_upper_idx, 3]/dU
-    stag_col = frac*panel_terms[last_upper_idx+1,:]+(1-frac)*panel_terms[last_upper_idx,:]
-
-    ## Split into upper and lower surface terms
-    # upper panel needs to be reversed since XFoil writes out starting with 
-    #   the upper surface trailing edge to lower surface trailing edge.
-    panel_upper_terms = panel_terms[last_upper_idx::-1, :]
-    # insert the stagnation point
-    panel_upper_terms = np.insert(panel_upper_terms, 0, stag_col, 0)
-    # adjust distance is based on arclength distance from upper trailing edge.
-    panel_upper_terms[:,0] = panel_upper_terms[0,0] - panel_upper_terms[:,0]
-    panel_upper_terms[:,3] = U_inf*panel_upper_terms[:,3]
-    upper_values = PanelValues(*panel_upper_terms.T)
-    
-    # lower panel
-    panel_lower_terms = panel_terms[last_upper_idx+1:, :]
-    # insert the stagnation point
-    panel_lower_terms = np.insert(panel_lower_terms, 0, stag_col, 0)
-    # adjust distance (see upper panel)
-    panel_lower_terms[:,0] = panel_lower_terms[:,0] - panel_lower_terms[0,0]
-    # adjust the signs of the lower surface terms
-    panel_lower_terms[:,3] = -panel_lower_terms[:,3]
-    panel_lower_terms[:,[6,10,11]] = np.abs(panel_lower_terms[:,[6,10,11]])
-    # remove scale of velocities
-    panel_lower_terms[:,3] = U_inf*panel_lower_terms[:,3]
-    lower_values = PanelValues(*panel_lower_terms.T)
-    
-    # wake
-    # adjust distance (base it on trailing edge)
-    wake_terms = np.asarray(wake_terms)
-    wake_terms[:,0] = wake_terms[:,0] - s_af
-    # remove zeros from skin friction coefficient
-    wake_terms = np.delete(wake_terms, 6, 1)
-    wake_values = WakeValues(*wake_terms.T)
-    
-    return c, U_inf, Re, upper_values, lower_values, wake_values
 
 
 if (__name__ == "__main__"):
---
+++ tests/Edland Figures/xfoil_interface.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  5 22:56:30 2022
+
+@author: ddmarshall
+"""
+
+import numpy as np
+import csv
+from collections import namedtuple
+
+
+def read_xfoil_dump_file(xfoil_file, U_inf=1):
+    """
+    Return the results from XFoil dump file.
+    
+    Args
+    ----
+        xfoi_file - file name containing XFoil dump data
+        
+    Returns
+    -------
+        s_lower - arclength distance from stagnation point on lower surface
+        s_upper - arclength distance from stagnation point on upper surface
+        Ue_lower - lower surface velocity
+        Ue_upper - upper surface velocity
+        del_star_lower - lower surface displacement thickness
+        del_star_upper - upper surface displacement thickness
+        theta_lower - lower surface momentum thickness
+        theta_upper - upper surface momentum thickness
+        cf_lower - lower surface skin friction coefficient
+        cf_upper - upper surface skin friction coefficient
+        H_lower - lower surface shape factor
+        H_upper - upper surface shape factor
+    """
+    
+    PanelValues = namedtuple('PanelValues', ['s', 'x', 'y', 'Ue', 'delta_d', 'delta_m', 'cf', 'H', 'Hstar', 'P', 'm', 'K'])
+    WakeValues = namedtuple('PanelValues', ['s', 'x', 'y', 'Ue', 'delta_d', 'delta_m', 'H'])
+    
+    xfoil_file = open(xfoil_file, 'r')
+    xfoil_reader = csv.reader(xfoil_file)
+    panel_terms = []
+    wake_terms = []
+    for row in xfoil_reader:
+        if (row[0][0] != '#'):
+            col = np.array(row[0].split(), 'float')
+            
+            if (np.size(col) == 12):
+                panel_terms.append(col)
+            elif (np.size(col) == 8):
+                wake_terms.append(col)
+            else:
+                raise Exception('Invalid number of columns for XFoil import')
+                    
+    ## Find stagnation point and the total arclength of airfoil
+    panel_terms = np.asarray(panel_terms)
+    last_upper_idx = np.where(np.sign(panel_terms[:-1, 3]) != np.sign(panel_terms[1:, 3]))[0][0]
+    s_af = panel_terms[-1, 0]
+    
+    # interpolate stagnation point
+    dU = panel_terms[last_upper_idx+1, 3]-panel_terms[last_upper_idx, 3]
+    frac = -panel_terms[last_upper_idx, 3]/dU
+    stag_col = frac*panel_terms[last_upper_idx+1,:]+(1-frac)*panel_terms[last_upper_idx,:]
+
+    ## Split into upper and lower surface terms
+    # upper panel needs to be reversed since XFoil writes out starting with 
+    #   the upper surface trailing edge to lower surface trailing edge.
+    panel_upper_terms = panel_terms[last_upper_idx::-1, :]
+    # insert the stagnation point
+    panel_upper_terms = np.insert(panel_upper_terms, 0, stag_col, 0)
+    # adjust distance is based on arclength distance from upper trailing edge.
+    panel_upper_terms[:,0] = panel_upper_terms[0,0] - panel_upper_terms[:,0]
+    panel_upper_terms[:,3] = U_inf*panel_upper_terms[:,3]
+    upper_values = PanelValues(*panel_upper_terms.T)
+    
+    # lower panel
+    panel_lower_terms = panel_terms[last_upper_idx+1:, :]
+    # insert the stagnation point
+    panel_lower_terms = np.insert(panel_lower_terms, 0, stag_col, 0)
+    # adjust distance (see upper panel)
+    panel_lower_terms[:,0] = panel_lower_terms[:,0] - panel_lower_terms[0,0]
+    # adjust the signs of the lower surface terms
+    panel_lower_terms[:,3] = -panel_lower_terms[:,3]
+    panel_lower_terms[:,[6,10,11]] = np.abs(panel_lower_terms[:,[6,10,11]])
+    # remove scale of velocities
+    panel_lower_terms[:,3] = U_inf*panel_lower_terms[:,3]
+    lower_values = PanelValues(*panel_lower_terms.T)
+    
+    # wake
+    # adjust distance (base it on trailing edge)
+    wake_terms = np.asarray(wake_terms)
+    wake_terms[:,0] = wake_terms[:,0] - s_af
+    # remove zeros from skin friction coefficient
+    wake_terms = np.delete(wake_terms, 6, 1)
+    wake_values = WakeValues(*wake_terms.T)
+    
+    return upper_values, lower_values, wake_values
+
+
