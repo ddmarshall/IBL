c1a4d82e407f8f492aabc0a4b9b65acb9a94a7ae
ddmarshall <ddmarshall@gmail.com>
2022-08-20 01:06:11 -0700

    Changed the setting BC method to automatically update solution

--- pyBL/blasius_solution.py
+++ pyBL/blasius_solution.py
@@ -19,43 +19,6 @@ class BlasiusSolution:
         self._eta_inf = eta_inf
         
         self._set_boundary_condition(fpp0)
-        self._set_solution()
-    
-    def _set_boundary_condition(self, fpp0 = None):
-        if fpp0 is None:
-            def fun(fpp0):
-                F0 = [0, 0, fpp0]
-                rtn = solve_ivp(fun = self._ode_fun,
-                                t_span = [0, self._eta_inf], y0 = F0,
-                                method = 'RK45', dense_output = False,
-                                events = None, rtol = 1e-8, atol = 1e-11)
-                if not rtn.success:
-                    raise ValueError("Could not find boundary condition")
-                return 1-rtn.y[1,-1]
-            
-            sol = root_scalar(fun, method = "bisect", bracket = [0.4, 0.5])
-            if sol.converged:
-                self._fpp0 = sol.root
-            else:
-                raise ValueError("Root finded could not find boundary "
-                                 "condition")
-        else:
-            self._fpp0 = fpp0
-    
-    def _set_solution(self):
-        F0 = [0, 0, self._fpp0]
-        
-        rtn = solve_ivp(fun = self._ode_fun, t_span = [0, self._eta_inf],
-                        y0 = F0, method = 'RK45', dense_output = True,
-                        events = None, rtol = 1e-8, atol = 1e-11)
-        
-        self._F = None
-        if rtn.success:
-            self._F=rtn.sol
-        else:
-            raise ValueError("Initial condition for solver, f\'\'(0)={0:.6f}, "
-                             "did not produce converged solution."
-                             "".format(self._fpp0))
     
     def f(self, eta):
         return self._F(eta)[0]
@@ -136,6 +99,43 @@ class BlasiusSolution:
     def D(self, x, rho):
         return 0.5*rho*self._nu*self._g(x)*self._U_ref**2*self.eta_k()
     
+    def _set_boundary_condition(self, fpp0 = None):
+        if fpp0 is None:
+            def fun(fpp0):
+                F0 = [0, 0, fpp0]
+                rtn = solve_ivp(fun = self._ode_fun,
+                                t_span = [0, self._eta_inf], y0 = F0,
+                                method = 'RK45', dense_output = False,
+                                events = None, rtol = 1e-8, atol = 1e-11)
+                if not rtn.success:
+                    raise ValueError("Could not find boundary condition")
+                return 1-rtn.y[1,-1]
+            
+            sol = root_scalar(fun, method = "bisect", bracket = [0.4, 0.5])
+            if sol.converged:
+                self._fpp0 = sol.root
+            else:
+                raise ValueError("Root finded could not find boundary "
+                                 "condition")
+        else:
+            self._fpp0 = fpp0
+        self._set_solution()
+    
+    def _set_solution(self):
+        F0 = [0, 0, self._fpp0]
+        
+        rtn = solve_ivp(fun = self._ode_fun, t_span = [0, self._eta_inf],
+                        y0 = F0, method = 'RK45', dense_output = True,
+                        events = None, rtol = 1e-8, atol = 1e-11)
+        
+        self._F = None
+        if rtn.success:
+            self._F=rtn.sol
+        else:
+            raise ValueError("Initial condition for solver, f\'\'(0)={0:.6f}, "
+                             "did not produce converged solution."
+                             "".format(self._fpp0))
+    
     def _g(self, x):
         return np.sqrt(0.5*self._U_ref/(self._nu*x))
     
@@ -148,5 +148,3 @@ class BlasiusSolution:
         Fp[2] = -F[0]*F[2]
         
         return Fp
-            
-
