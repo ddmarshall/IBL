b45ecb1fdbfd348fa6e4d9f6c67669ebe8a8d46d
ddmarshall <ddmarshall@gmail.com>
2022-08-22 22:20:13 -0700

    Added nonlinear Thwaites method to examples

--- examples/thwaites_falkner_skan_wedge_flow_comparison.py
+++ examples/thwaites_falkner_skan_wedge_flow_comparison.py
@@ -16,6 +16,7 @@ from matplotlib.gridspec import GridSpec
 
 from pyBL.falkner_skan_solution import FalknerSkanSolution
 from pyBL.thwaites_method import ThwaitesMethodLinear
+from pyBL.thwaites_method import ThwaitesMethodNonlinear
 
 
 def compare_stagnation_solution():
@@ -56,115 +57,163 @@ def compare_stagnation_solution():
         print("Could not get solution for Thwaites method: " + rtn.message)
         return
     
+    tmn = ThwaitesMethodNonlinear(U_e = U_e_fun, dU_edx = dU_edx_fun,
+                                  d2U_edx2 = d2U_edx2_fun,
+                                  data_fits = "Spline")
+    tmn.set_solution_parameters(x0 = x[0], x_end = x[-1],
+                                delta_m0 = fs.delta_m(x[0]), nu = nu_inf)
+    rtn = tmn.solve()
+    if not rtn.success:
+        print("Could not get solution for Thwaites method: " + rtn.message)
+        return
+    
     ## Calculate the boundary layer parameters
     delta_d_exact = fs.delta_d(x)
     delta_d_standard = tml.delta_d(x)
+    delta_d_nonlinear = tmn.delta_d(x)
     delta_m_exact = fs.delta_m(x)
     delta_m_standard = tml.delta_m(x)
+    delta_m_nonlinear = tmn.delta_m(x)
     c_f_exact = fs.tau_w(x, rho_inf)/(0.5*rho_inf*U_inf**2)
     c_f_standard = tml.tau_w(x, rho_inf)/(0.5*rho_inf*U_inf**2)
+    c_f_nonlinear = tmn.tau_w(x, rho_inf)/(0.5*rho_inf*U_inf**2)
     H_d_exact = fs.H_d(x)
     H_d_standard = tml.H_d(x)
+    H_d_nonlinear = tmn.H_d(x)
     U_n_exact = fs.V_e(x)
     U_n_standard = tml.U_n(x)
+    U_n_nonlinear = tmn.U_n(x)
     
     ## plot functions
-    plt.rcParams['figure.figsize'] = [8, 5]
-    
     # Plot the quad plot with boundary layer parameters
-    fig = plt.figure(constrained_layout = True)
-    fig.set_figwidth(8)
-    fig.set_figheight(10)
-    gs = GridSpec(3, 2, figure = fig)
-    axis_delta_m = fig.add_subplot(gs[0, 0])
-    axis_delta_d = fig.add_subplot(gs[0, 1])
-    axis_c_f = fig.add_subplot(gs[1, 0])
-    axis_H_d = fig.add_subplot(gs[1, 1])
-    axis_error = fig.add_subplot(gs[2, :])
+    fig = plt.figure()
+    fig.set_figwidth(10)
+    fig.set_figheight(15)
+    gs = GridSpec(5, 2, figure = fig)
+    axis_delta_d = fig.add_subplot(gs[0, 0])
+    axis_delta_d_error = fig.add_subplot(gs[0, 1])
+    axis_delta_m = fig.add_subplot(gs[1, 0])
+    axis_delta_m_error = fig.add_subplot(gs[1, 1])
+    axis_H_d = fig.add_subplot(gs[2, 0])
+    axis_H_d_error = fig.add_subplot(gs[2, 1])
+    axis_c_f = fig.add_subplot(gs[3, 0])
+    axis_c_f_error = fig.add_subplot(gs[3, 1])
+    axis_U_n = fig.add_subplot(gs[4, 0])
+    axis_U_n_error = fig.add_subplot(gs[4, 1])
     
     exact_color = "black"
-    exact_label = "Falkner-Skan"
+    exact_label = "Blasius"
     standard_color = "green"
     standard_label = "Standard"
+    nonlinear_color = "red"
+    nonlinear_label = "Nonlinear"
     
-    # Momentum thickness in 0,0
-    ax = axis_delta_m
-    exact_curve = ax.plot(x/c, delta_m_exact/c, color=exact_color)
-    standard_curve = ax.plot(x/c, delta_m_standard/c, color=standard_color)
-    ax.set_ylim(0, 0.0004)
-    ax.set_xlabel(r"$x/c$")
-    ax.set_ylabel(r"$\delta_m/c$")
-    ax.grid(True)
-    
-    # Displacement thickness in 0,1
+    # Displacement thickness in 0,:
     ax = axis_delta_d
-    ax.plot(x/c, delta_d_exact/c, color=exact_color)
-    ax.plot(x/c, delta_d_standard/c, color=standard_color)
+    exact_curve = ax.plot(x/c, delta_d_exact/c, color = exact_color,
+                          label = exact_label)
+    standard_curve = ax.plot(x/c, delta_d_standard/c, color = standard_color,
+            label = standard_label)
+    nonlinear_curve = ax.plot(x/c, delta_d_nonlinear/c, color = nonlinear_color,
+            label = nonlinear_label)
     ax.set_ylim(0, 0.0008)
-    ax.set_xlabel(r"$x/c$")
     ax.set_ylabel(r"$\delta_d/c$")
     ax.grid(True)
     
-    # Skin friction coefficient in 1,0
-    ax = axis_c_f
-    ax.plot(x/c, c_f_exact, color=exact_color)
-    ax.plot(x/c, c_f_standard, color=standard_color)
-    ax.set_ylim(0.001, 0.002)
-    ax.set_xlabel(r"$x$")
-    ax.set_ylabel(r"$c_f$")
+    ax = axis_delta_d_error
+    ax.plot(x/c, np.abs(1-delta_d_standard/delta_d_exact),
+            color = standard_color)
+    ax.plot(x/c, np.abs(1-delta_d_nonlinear/delta_d_exact),
+            color = nonlinear_color)
+    ax.set_ylabel("Relative Error")
+    ax.set_ylim([1e-4,1])
+    ax.set_yscale('log')
     ax.grid(True)
     
-    # Shape factor in 1,1
+    # Momentum thickness in 1,:
+    ax = axis_delta_m
+    ax.plot(x/c, delta_m_exact/c, color = exact_color)
+    ax.plot(x/c, delta_m_standard/c, color = standard_color)
+    ax.plot(x/c, delta_m_nonlinear/c, color = nonlinear_color)
+    ax.set_ylim(0, 0.0004)
+    ax.set_ylabel(r"$\delta_m/c$")
+    ax.grid(True)
+    
+    ax = axis_delta_m_error
+    ax.plot(x/c, np.abs(1-delta_m_standard/delta_m_exact),
+            color = standard_color)
+    ax.plot(x/c, np.abs(1-delta_m_nonlinear/delta_m_exact),
+            color = nonlinear_color)
+    ax.set_ylabel("Relative Error")
+    ax.set_ylim([1e-4,1])
+    ax.set_yscale('log')
+    ax.grid(True)
+    
+    # Displacement shape factor in 2,:
     ax = axis_H_d
-    ax.plot(x/c, H_d_exact, color=exact_color)
-    ax.plot(x/c, H_d_standard, color=standard_color)
+    ax.plot(x/c, H_d_exact, color = exact_color)
+    ax.plot(x/c, H_d_standard, color = standard_color)
+    ax.plot(x/c, H_d_nonlinear, color = nonlinear_color)
     ax.set_ylim(2.2, 2.5)
-    ax.set_xlabel(r"$x/c$")
     ax.set_ylabel(r"$H_d$")
     ax.grid(True)
     
-    fig.legend(handles = [exact_curve[0], standard_curve[0]], 
-               labels = [exact_label, standard_label],
-               loc = "upper center", bbox_to_anchor = (0.55, 0.36),
-               ncol = 2, borderaxespad = 0.1)
-    
-    # Plot error compared to the Blasius result
-    ax = axis_error
-    ax.plot(x/c, np.abs(1-delta_m_standard/delta_m_exact),
-            label = r"$\delta_m$: " + standard_label);
-    ax.plot(x/c, np.abs(1-delta_d_standard/delta_d_exact),
-            label = r"$\delta_d$: " + standard_label);
-    ax.plot(x/c, np.abs(1-c_f_standard/c_f_exact),
-            label = r"$c_f$: " + standard_label);
+    ax = axis_H_d_error
     ax.plot(x/c, np.abs(1-H_d_standard/H_d_exact),
-            label = r"$H_d$: " + standard_label);
+            color = standard_color)
+    ax.plot(x/c, np.abs(1-H_d_nonlinear/H_d_exact),
+            color = nonlinear_color)
+    ax.set_ylabel("Relative Error")
+    ax.set_ylim([1e-4,1])
+    ax.set_yscale('log')
+    ax.grid(True)
+    
+    # Skin friction coefficient in 3,:
+    ax = axis_c_f
+    ax.plot(x/c, c_f_exact, color = exact_color)
+    ax.plot(x/c, c_f_standard, color = standard_color)
+    ax.plot(x/c, c_f_nonlinear, color = nonlinear_color)
+    ax.set_ylim(0.001, 0.002)
+    ax.set_ylabel(r"$c_f$")
+    ax.grid(True)
+    
+    ax = axis_c_f_error
+    ax.plot(x/c, np.abs(1-c_f_standard/c_f_exact),
+            color = standard_color)
+    ax.plot(x/c, np.abs(1-c_f_nonlinear/c_f_exact),
+            color = nonlinear_color)
+    ax.set_ylabel("Relative Error")
+    ax.set_ylim([1e-4,1])
+    ax.set_yscale('log')
+    ax.grid(True)
+    
+    # Transpiration velocity in 4,:
+    ax = axis_U_n
+    ax.plot(x/c, U_n_exact/U_inf, color = exact_color)
+    ax.plot(x/c, U_n_standard/U_inf, color = standard_color)
+    ax.plot(x/c, U_n_nonlinear/U_inf, color = nonlinear_color)
+    ax.set_ylim(0, 0.01)
+    ax.set_xlabel(r"$x/c$")
+    ax.set_ylabel(r"$U_n/U_\infty$")
+    ax.grid(True)
+    
+    ax = axis_U_n_error
+    ax.plot(x/c, np.abs(1-U_n_standard/U_n_exact),
+            color = standard_color)
+    ax.plot(x/c, np.abs(1-U_n_nonlinear/U_n_exact),
+            color = nonlinear_color)
     ax.set_xlabel(r"$x/c$")
     ax.set_ylabel("Relative Error")
     ax.set_ylim([1e-4,1])
     ax.set_yscale('log')
     ax.grid(True)
-    ax.legend(ncol=4, loc = "lower center")
-    plt.show()
     
-    # Plot Transpiration velocity
-    fig, ax = plt.subplots(nrows=2, ncols=1, sharex='all')
-    fig.set_figwidth(9)
-    fig.set_figheight(7)
-    
-    ax[0].plot(x/c, U_n_exact/U_inf, label = exact_label, color = exact_color);
-    ax[0].plot(x/c, U_n_standard/U_inf, label = standard_label, color = standard_color);
-    ax[0].set_xlabel(r"$x/c$")
-    ax[0].set_ylabel(r"$U_n/U_e$")
-    ax[0].set_ylim(0, 0.01)
-    ax[0].grid(True)
-    
-    ax[1].plot(x/c, np.abs(1-U_n_standard/U_n_exact))
-    ax[1].set_xlabel(r"$x/c$")
-    ax[1].set_ylabel("Relative Error")
-    ax[1].set_ylim([1e-4,1])
-    ax[1].set_yscale('log')
-    ax[1].grid(True)
-    plt.show()
+    fig.subplots_adjust(bottom=0.075, wspace=0.5)
+    fig.legend(handles = [exact_curve[0], standard_curve[0],
+                          nonlinear_curve[0]], 
+               labels = [exact_label, standard_label, nonlinear_label],
+               loc = "upper center", bbox_to_anchor = (0.45, 0.03),
+               ncol = 3, borderaxespad = 0.1)
 
 
 if (__name__ == "__main__"):
