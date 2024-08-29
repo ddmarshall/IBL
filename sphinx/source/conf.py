140f8eeb95af662529318744d2fd9921228da1c7
David D. Marshall <ddmarshall@gmail.com>
2024-08-29 16:24:58 -0700

    Fixed name and layout of ibl module for documentation.

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -4,11 +4,12 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # pylint: skip-file
+# type: ignore
 
 # -- Path setup --------------------------------------------------------------
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../ibl'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
